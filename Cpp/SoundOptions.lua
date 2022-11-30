3c49890d83a95e61001d4f44ac23a6ef5be28757
mcb <mgrbo@web.de>
2022-11-30 20:20:47 +0100

    soundoptions

--- Cpp/SoundOptions.lua
+++ Cpp/SoundOptions.lua
@@ -2,5 +2,6 @@ SoundOptions = {
     HasEAX = function()end,
     HasHardware3dSound = function()end,
     InitSoundDefaultValues = function()end,
+    ---reoads sound volumes from GDB
     UpdateSound = function()end,
 }
