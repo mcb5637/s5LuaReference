0bf9cacff9736e6fdff57fb54e0a0c958614a3cd
maxthi <MadShadow@protonmail.com>
2023-11-26 14:10:26 +0100

    Create Explore.lua

---
+++ S5/Explore.lua
@@ -0,0 +1,12 @@
+Explore = {};
+
+--- Create an exploration or move to another position.
+--- @param _Name string Name of exploration.
+--- @param _Where Position Position table, id or entity name.
+--- @param _Range number Range in centimeter.
+function Explore.Show(_Name, _Where, _Range) end
+
+--- Destroy an exploration.
+--- @param _Name string Name of exploration.
+function Explore.Hide(_Name) end
+
